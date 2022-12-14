d61a6909687bfc45b1026c84871a636fc2ef6cae
Y-Moshe <moshe.nehemiah@gmail.com>
2022-12-14 14:24:31 +0200

    add fonts, define global icon cmp

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/styles/main.scss'
+import './assets/styles/setup/_typography.scss'
+import icon from './components/icon.vue'
 
 const app = createApp(App)
 
+app.component('icon', icon)
 app.use(router)
 
 app.mount('#app')
